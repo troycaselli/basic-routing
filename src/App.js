679a5a29161ca04bdad213378f0ec9c080bc3a3b
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-09-29 15:30:50 -0700

    final edit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
 
   return (
     <div className="App">
-      <p>TEST</p>
       <header className="App-header">
           <Link to='/'>Home</Link>
           <Link to='/about'>About</Link>
